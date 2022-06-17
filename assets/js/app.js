2c1c89cb629a16b859142481f17fe87653fa41c0
MisterSilvereagle <MisterSilvereagle@gmx.de>
2022-06-17 15:48:03 +0200

    header now working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,4 @@
-import headerComp from '../vue/header.vue'
-
 export default {
-	components: {
-		headerComp
-	},
 	methods: {
 		answer(ans) {
 			if(!this.show_next_button) {
@@ -219,7 +214,8 @@ export default {
 			correct_banner: false,
 			wrong_banner: false,
 			vars: [],
-			results: []
+			results: [],
+			template1: '<span class="ms-font home"><a href="." class="nolink"><img src="assets/img/ms_logo.png" class="img"><div class="noblock">Zum Start</div></a><span class="centered-right"><span class="info-bar-wrapper"><span class="info-bar-element"><span class="info-text"><a href="https://github.com/baum22team/">baum22team</a></span><img src="assets/img/baum22team.jpeg" class="info-bar-picture"></span><br><span class="info-bar-element"><span class="info-text"><a href="https://github.com/mistersilvereagle/">MisterSilvereagle</a></span><img src="assets/img/mse.png" class="info-bar-picture"></span><br><span class="info-bar-element"><span class="info-text"><a href="https://github.com/lefa22/">lefa22</a></span><img src="assets/img/lefa22.png" class="info-bar-picture"></span><br><span class="info-bar-element"><span class="info-text"><a href="https://hardenberg-gymnasium.de/ak-medienscouts/">Medienscouts</a></span><img src="assets/img/ms_logo.png" class="info-bar-picture"></span><br><span class="info-bar-element"><span class="info-text"><a href="https://github.com/">Github</a></span><img src="assets/img/github.png" class="info-bar-picture"></span></span></span></span>'
 		}
 	},
 	computed: {
