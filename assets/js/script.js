3ff8608f7ccd73da724d5eba3f72e0899f061238
MisterSilvereagle <58659498+MisterSilvereagle@users.noreply.github.com>
2023-07-11 08:23:59 +0200

    Fix alphabet here

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ function get_results(correct_answers) {
 	var numbers = window.location.href.slice(window.location.href.indexOf('?s=') + 3).split("&")[0];
 	var ar = [];
 	for(let i = 0; i < numbers.length; i++) {
-		ar.push("0123456789abcdefghijklmnopqrstuvwxyz".indexOf(numbers[i]));
+		ar.push("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(numbers[i]));
 	}
 
 	var rlytrue = 0;
